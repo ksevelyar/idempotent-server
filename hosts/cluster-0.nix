baf86d536ea8f7eeffc224ac1b4a5236b7b17c6a
Sergey Zubkov <ksevelyar@protonmail.com>
2026-05-29 17:28:05 +0300

    habits: enable upgrade for websocket

--- hosts/cluster-0.nix
+++ hosts/cluster-0.nix
@@ -131,6 +131,10 @@
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_read_timeout 1d;
       '';
     };
   };
