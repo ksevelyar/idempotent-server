5040a9de9787db9f6125548b26e6d4e3a21a683f
Sergey Zubkov <ksevelyar@protonmail.com>
2026-06-02 01:10:20 +0300

    update habits; increase curl timeout from 5s to 15s

--- sys/nix.nix
+++ sys/nix.nix
@@ -8,15 +8,15 @@
     settings = {
       experimental-features = ["nix-command" "flakes"];
 
-      # NOTE: fail curl after 5 seconds, rebuild from source if fetching cache fails
-      connect-timeout = 5;
-      stalled-download-timeout = 5;
+      # NOTE: fail curl after 15 seconds, rebuild from source if fetching cache fails
+      connect-timeout = 15;
+      stalled-download-timeout = 15;
       fallback = true;
 
-      substituters = [
+      extra-substituters = [
         "https://idempotent-server.cachix.org"
       ];
-      trusted-public-keys = [
+      extra-trusted-public-keys = [
         "idempotent-server.cachix.org-1:0QGOQVYN9UD8Oeb32IcipuxNuM7fIo/+aw/ZDOB5Tos="
       ];
     };
